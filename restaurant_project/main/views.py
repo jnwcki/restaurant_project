245c6aeb34f59be2d55119613846ba1ade05d94d
joegotflow83 <joe@absolutod.com>
2016-03-12 14:02:41 -0500

    Added signup view

--- restaurant_project/main/views.py
+++ restaurant_project/main/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class Signup(CreateView):
+    """Allow a user to signup"""
+    model = User
+    form = NewUserCreationForm
+
+    def form_valid(self, form):
+        """Validate the form"""
+        new_user = form.commit(save=False)
+        new_user.user = new_user
+        new_user.first_name = form.cleaned_data['first_name']
+        new_user.last_name = form.cleaned_data['last_name']
+        new_user.save()
+        return super().form_valid(form)
